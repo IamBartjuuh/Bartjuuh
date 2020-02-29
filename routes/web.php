58a93991f346bb17b63266ddc8e59e74278d358c
IamBartjuuh <bartjuuhyt@gmail.com>
2020-02-29 14:53:20 +0100

    Last fix this solves problems

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,23 @@ Auth::routes();
 
 //Guest Pages
 Route::get('/', 'PagesController@index')->name('index');
-Route::get('/dashboard', 'ProjectController@home')->name('home')->middleware('authuser');
+Route::get('/dashboard', 'ProjectController@home')->name('home')->middleware('auth');
 Route::get('/project/{id}', 'ProjectController@project');
 Route::get('/projecten', 'ProjectController@projectenpage');
 //Admin Pages
 
 //Make Project
-Route::get('/makeproject', 'ProjectController@createget')->middleware('authuser');
-Route::post('/newproject', 'ProjectController@createpost')->middleware('authuser');
+Route::get('/makeproject', 'ProjectController@createget')->middleware('auth');
+Route::post('/newproject', 'ProjectController@createpost')->middleware('auth');
 //Change Project
-Route::get('/project/{id}/aanpassen', 'ProjectController@changeproject')->middleware('authuser');
-Route::post('/project/{id}/aanpassen', 'ProjectController@changepost')->middleware('authuser');
+Route::get('/project/{id}/aanpassen', 'ProjectController@changeproject')->middleware('auth');
+Route::post('/project/{id}/aanpassen', 'ProjectController@changepost')->middleware('auth');
 //Make BlogPost
-Route::get('/project/{id}/update', 'UpdateController@getupdate')->middleware('authuser');
-Route::post('/project/{id}/update', 'UpdateController@postupdate')->middleware('authuser');
+Route::get('/project/{id}/update', 'UpdateController@getupdate')->middleware('auth');
+Route::post('/project/{id}/update', 'UpdateController@postupdate')->middleware('auth');
 //Change BlogPost & Delete
-Route::get('/blogpost/{id}/change', 'UpdateController@getchange')->middleware('authuser');
-Route::post('/blogpost/{id}/change', 'UpdateController@postchange')->middleware('authuser');
+Route::get('/blogpost/{id}/change', 'UpdateController@getchange')->middleware('auth');
+Route::post('/blogpost/{id}/change', 'UpdateController@postchange')->middleware('auth');
 //Delete BlogPost
-Route::post('/blogpost/delete/{id}', 'UpdateController@deletepost')->middleware('authuser');
+Route::post('/blogpost/delete/{id}', 'UpdateController@deletepost')->middleware('auth');
 
